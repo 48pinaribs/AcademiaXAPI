baf7127b7ec74e17fcc109e58309d288893b4efd
pinnotr <99140556+pinnotr@users.noreply.github.com>
2025-03-26 23:41:26 +0300

    feat: Add UserType enum for user role management

---
+++ AcademiaX_Data_Access/Enums/UserType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademiaX_Data_Access.Enums
+{
+	public enum UserType
+	{
+		Student,
+		Teacher,
+		Administrator
+	}
+}
