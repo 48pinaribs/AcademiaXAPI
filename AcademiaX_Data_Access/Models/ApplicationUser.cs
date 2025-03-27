0173abf3553f530b48719a0d158831349544db97
pinnotr <99140556+pinnotr@users.noreply.github.com>
2025-03-27 14:14:44 +0300

    feat: Add core properties for Student ,Teacher,Administrator entity

--- AcademiaX_Data_Access/Models/ApplicationUser.cs
+++ AcademiaX_Data_Access/Models/ApplicationUser.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AcademiaX_Data_Access.Domain;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,14 @@ namespace AcademiaX_Data_Access.Models
 {
 	public class ApplicationUser : IdentityUser
 	{
+		public string? FullName { get; set; }
+		public string? ProfilePicture { get; set; }
+		public DateTime DateOfBirth { get; set; }
+		public ICollection<Announcement> Announcements { get; set; }
+		public ICollection<Attendance> Attendances { get; set; }
+		public ICollection<Course> Courses { get; set; }
+		public ICollection<Grade> Grades { get; set; }
+		public ICollection<Message> Messages { get; set; }
 
 	}
 }
