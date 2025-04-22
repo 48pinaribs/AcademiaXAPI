70d168a83bd2971292357df211030b73fba316fd
pinnotr <99140556+pinnotr@users.noreply.github.com>
2025-04-22 22:04:28 +0300

    feat(mapper): configure AutoMapper profile for RegisterRequestDTO and ApplicationUser mapping

---
+++ AcademiaX_Business/Mapper/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AcademiaX_Business.Dtos;
+using AcademiaX_Data_Access.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademiaX_Business.Mapper
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			CreateMap<RegisterRequestDTO, ApplicationUser>().ReverseMap();
+		}
+	}
+}
