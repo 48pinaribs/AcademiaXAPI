cb3590641c7e0a79cf1fe33abf740aae00d674cd
pinnotr <99140556+pinnotr@users.noreply.github.com>
2025-04-22 15:44:31 +0300

    Create LoginRequestDTO and RegisterRequestDTO classes for user authentication

---
+++ AcademiaX_Business/Dtos/LoginRequestDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AcademiaX_Business.Dtos
+{
+	public class LoginRequestDTO
+	{
+	
+			[Required(ErrorMessage = "User ID is required.")]
+			public string Identifier { get; set; }
+			// Öğrenci numarası, öğretmen ID'si ya da admin ID'si
+
+			[Required(ErrorMessage = "Password is required.")]
+			[MinLength(6, ErrorMessage = "Password must be at least 6 characters.")]
+			public string Password { get; set; }
+
+			[Required(ErrorMessage = "User type must be specified.")]
+			[RegularExpression("Student|Teacher|Administrator", ErrorMessage = "User type can only be 'Student', 'Teacher' or 'Administrator'.")]
+			public string UserType { get; set; }
+			// Student, Teacher, Administrator
+		}
+	
+}
---
+++ AcademiaX_Business/Dtos/ReqisterRequestDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademiaX_Business.Dtos
+{
+	public class RegisterRequestDTO
+	{
+			public string UserName { get; set; }
+			public string Fullname { get; set; }
+			public string Password { get; set; }
+			public string ConfirmPassword { get; set; }
+			public string UserType { get; set; } // Student, Teacher, Admin
+			public string Email { get; set; }
+			public string PhoneNumber { get; set; }
+			public DateTime? BirthDate { get; set; }
+			public string SecurityQuestion { get; set; }
+			public string SecurityAnswer { get; set; }
+		
+	}
+}
