9d7b568338086666e8aa620d5b0db1d44affa48c
pinnotr <99140556+pinnotr@users.noreply.github.com>
2025-04-22 15:39:49 +0300

    Create UserService class implementing IUserService with Login and Register methods

---
+++ AcademiaX_Business/Concrete/UserService.cs
@@ -0,0 +1,28 @@
+﻿using AcademiaX_Business.Abstraction;
+using AcademiaX_Business.Dtos;
+using AcademiaX_Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademiaX_Business.Concrete
+{
+	public class UserService : IUserService
+	{
+		public UserService()
+		{
+		}
+
+		public Task<ApiResponse> Login(LoginRequestDTO model)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ApiResponse> Register(RegisterRequestDTO model)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
