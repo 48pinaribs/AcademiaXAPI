43f946b6fddd4f418a544ef96856bdc58c9a89ac
pinnotr <99140556+pinnotr@users.noreply.github.com>
2025-04-24 16:47:17 +0300

    refactor: Removed unused fields from LoginRequestDTO

--- AcademiaX_Business/Dtos/LoginRequestDTO.cs
+++ AcademiaX_Business/Dtos/LoginRequestDTO.cs
@@ -13,10 +13,6 @@ namespace AcademiaX_Business.Dtos
 			[MinLength(6, ErrorMessage = "Password must be at least 6 characters.")]
 			public string Password { get; set; }
 
-			[Required(ErrorMessage = "User type must be specified.")]
-			[RegularExpression("Student|Teacher|Administrator", ErrorMessage = "User type can only be 'Student', 'Teacher' or 'Administrator'.")]
-			public string UserType { get; set; }
-			// Student, Teacher, Administrator
 		}
 	
 }
